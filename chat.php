dde9300322f2d32b8e9f8137255ae9f66fba372b
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2019-11-27 13:39:48 +0100

    Uitwerking van de exercise

--- chat.php
+++ chat.php
@@ -1,5 +1,55 @@
 <?php
-if (isset($_GET["name"])) {
+if (isset($_GET["chat"])){
+		$c = strtolower($_GET["chat"]);
+		if (strpos($c, "bereken voor mij") === true || strpos($c, "wat is") === true || strpos($c, "hoeveel is") === true || strpos($c, "bereken het volgende") === true || strpos($c, "calculate") === true){
+		$c = str_replace("bereken voor mij ", "", $c);
+		$c = str_replace("wat is ", "", $c);
+		$c = str_replace("hoeveel is ", "", $c);
+		$c = str_replace("bereken het volgende ", "", $c);
+		$c = str_replace("calculate ", "", $c);
+		
+		if (strpos($c, "+") === true){
+			$d = explode("+", $c);
+			$e = 0;
+			foreach ($d as $value) {
+				$e += $value;
+			}
+		}
+		elseif (strpos($c, "-") === true){
+			$d = explode("-", $c);
+			$e = $d[0];
+			array_shift($d);
+			foreach ($d as $value) {
+				$e -= $value;
+			}
+		}
+		elseif (strpos($c, "*") === true){
+			$d = explode("*", $c);
+			$e = $d[0];
+			array_shift($d);
+			foreach ($c as $value) {
+				$e *= $value;
+			}
+		}
+		elseif (strpos($c, "/") === true){
+			$d = explode("/", $c);
+			$e = $d[0];
+			array_shift($d);
+			foreach ($d as $value) {
+				$e /= $value;
+			}
+		}
+		elseif (strpos($c, "%") === true){
+			$d = explode("%", $c);
+			$e = $d[0];
+			array_shift($d);
+			foreach ($d as $value) {
+				$e %= $value;
+			}
+		}
+	}
+}
+elseif (isset($_GET["name"])) {
 	setcookie("name", $_GET["name"], time() + 3600);
 	header("Location: chat.php");
 }
@@ -20,6 +70,12 @@ else {
  <input type="text" id="msg">
  <button onclick="sendMsg();">send</button>
  <script>
+ 	function httpGet(theUrl){
+ 		var xmlHttp = new XMLHttpRequest();
+ 		xmlHttp.open("GET", theUrl, false);
+ 		xmlHttp.send(null);
+ 		return xmlHttp.responseText;
+ 	}
  	function sendMsg(){
  		var msg = document.getElementById("msg").value;
  		var chat = document.getElementById("chat").value;
@@ -29,6 +85,7 @@ else {
  		var usr = "<?php echo $_COOKIE["name"]; ?>";
  		document.getElementById("chat").value = chat + "\n" + hours + ":" + min + ":" + sec + " - " + usr + ": " + msg;
  		document.getElementById("msg").value	 = "";
+ 		httpGet("chat.php?chat=" + msg);
  		var a = document.getElementById("chat");
  		a.scrollTop = a.scrollHeight;
  	}
